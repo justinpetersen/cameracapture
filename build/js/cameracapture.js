c32164e4a4420fc4f3ea071d5f45a0dc91cfe48d
Justin Petersen <justin@justinpetersen.com>
2012-12-31 15:59:15 -0800

    Refactored demo code to be more OOP

--- build/js/cameracapture.js
+++ build/js/cameracapture.js
@@ -1,29 +1,49 @@
-// Put event listeners into place
-window.addEventListener("DOMContentLoaded", function() {
-  // Grab elements, create settings, etc.
-  var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    video = document.getElementById("video"),
-    videoObj = { "video": true },
-    errBack = function(error) {
-      console.log("Video capture error: ", error.code); 
-    };
+function CameraCapture( ) {
+	
+	this.canvas = null;
+	this.context = null;
+	this.video = null;
+	this.videoObj = null;
+	
+	this.errorHandler = function( ) {
+		console.log( "Video capture error: ", error.code );
+	};
+	
+	this.onSnapPhotoClickHandler = function( ) {	
+		this.context.drawImage( this.video, 0, 0, 640, 480 );
+	};
+	
+	this.init = function( ) {
+		this.canvas = document.getElementById("canvas");
+		this.context = canvas.getContext("2d");
+		this.video = document.getElementById("video");
+		this.videoObj = { "video": true };
+		
+		this.getUserMedia( );
 
-  // Put video listeners into place
-  if(navigator.getUserMedia) { // Standard
-    navigator.getUserMedia(videoObj, function(stream) {
-      video.src = stream;
-      video.play();
-    }, errBack);
-  } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-    navigator.webkitGetUserMedia(videoObj, function(stream){
-      video.src = window.webkitURL.createObjectURL(stream);
-      video.play();
-    }, errBack);
-  }
+		// Trigger photo take
+		document.getElementById( "snap" ).addEventListener( "click", $.proxy( this.onSnapPhotoClickHandler, this ) );
+	};
+	
+	this.getUserMedia = function( ) {
+		if ( navigator.getUserMedia ) { // Standard
+	    	navigator.getUserMedia( videoObj, function( stream ) {
+				this.video.src = stream;
+				this.video.play();
+			}, $.proxy( this.errorHandler, this ) );
+		} else if ( navigator.webkitGetUserMedia ) { // WebKit-prefixed
+			navigator.webkitGetUserMedia( this.videoObj, function( stream ) {
+				this.video.src = window.webkitURL.createObjectURL( stream );
+				this.video.play();
+			}, $.proxy( this.errorHandler, this ) );
+		}
+	};
+	
+}
 
-	// Trigger photo take
-	document.getElementById("snap").addEventListener("click", function() {
-	  context.drawImage(video, 0, 0, 640, 480);
-	});
-}, false);
+$( document ).ready( function( ) {
+	
+	var cameraCapture = new CameraCapture( );
+	cameraCapture.init( );
+	
+});
